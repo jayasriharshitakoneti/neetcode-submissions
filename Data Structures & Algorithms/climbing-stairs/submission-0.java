b02f6d0cac59f6597cdc6b31804a3c5ac50be029
Jaya Sriharshita Koneti <75335955+jayasriharshitakoneti@users.noreply.github.com>
2026-06-16 10:47:13 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] steps=new int[n+1];
+        steps[0]=1;
+        steps[1]=1;
+        for(int i=2;i<n+1;i++){
+            steps[i]=steps[i-1]+steps[i-2];
+        }
+        return steps[n];
+    }
+}
